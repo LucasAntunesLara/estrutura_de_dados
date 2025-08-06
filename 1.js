ae62db1e6082b35b686adfe1b496afd657e15a59
LUCAS LARA <lucas.060406@bento.local>
2025-08-06 03:50:30 -0300

    first commit

---
+++ 1.js
@@ -0,0 +1,9 @@
+const vetor = [3, 4, 2, 6, 9, 7]
+
+//FILTER
+//Elemento => começa em 1 (valor + posição)
+//Valor (que está em uma posição)
+//Posição => começa em 0
+vetor.filter((valor, posicao, copia) => {
+
+})
