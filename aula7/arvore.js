425e3d0d26aef89b77a4d987736c8d92e0086def
LucasAntunesLara <lucasantuneslara06@gmail.com>
2025-09-17 21:09:14 -0300

    aula 7 - método includes() incompleto

--- aula7/arvore.js
+++ aula7/arvore.js
@@ -19,28 +19,66 @@ class BinaryTree {
       return true
     }
 
-    let node_atual = this.root
+    let nodeAtual = this.root
 
     while (true) {
-      if (node_atual.value === value) return false
+      if (nodeAtual.value === value) return false
 
-      if (value > node_atual.value) {
-        if (node_atual.right === null) return (node_atual.right = node)
+      if (value > nodeAtual.value) {
+        if (nodeAtual.right === null) return (nodeAtual.right = node)
 
-        node_atual = node_atual.right
+        nodeAtual = nodeAtual.right
       } else {
-        if (node_atual.left === null) return (node_atual.left = node)
+        if (nodeAtual.left === null) return (nodeAtual.left = node)
 
-        node_atual = node_atual.left
+        nodeAtual = nodeAtual.left
       }
     }
+  }
+
+  includes(value) {
+    let nodeAtual = this.root
+
+    let resposta = null
+
+    while (resposta === null) {
+      //   console.log(typeof value)
+      console.log(typeof nodeAtual.value)
+      if (nodeAtual.value === value) resposta = true
+      else {
+        if (value > nodeAtual.value) {
+          // if (nodeAtual.right > value) return (nodeAtual.right = node)
+
+          nodeAtual = nodeAtual.right
+        } else {
+          //if (value < nodeAtual.value) {
+          nodeAtual = nodeAtual.left
+          // resposta = false
+        }
+      }
+
+      //   else {
+      // if (nodeAtual.left === null) return (nodeAtual.left = node)
+
+      // nodeAtual = nodeAtual.left
+      //   }
+    }
+
+    return resposta
   }
 }
 
 const tree = new BinaryTree()
 tree.insert(5)
-console.log(tree.insert(7))
+tree.insert(7)
 tree.insert(9)
 tree.insert(3)
+// tree.insert(0)
+
+console.log('5: ', tree.includes(5))
+console.log('7: ', tree.includes(7))
+console.log('9: ', tree.includes(9))
+console.log('3: ', tree.includes(3))
+console.log('0: ', tree.includes(0))
 
 console.log('\n\n')
