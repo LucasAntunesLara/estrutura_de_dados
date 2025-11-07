231057b41a8b3136b3f2cb2ee52db6d2cb45a791
LucasAntunesLara <lucasantuneslara06@gmail.com>
2025-11-07 10:14:30 -0300

    update avl

--- aula10/avl.js
+++ aula10/avl.js
@@ -1,92 +1,94 @@
-class Node {
+class No {
   constructor(value) {
-    this.value = value
-    this.left = null
-    this.right = null
-    this.height = 1
+    this.valor = value
+    this.esquerda = null
+    this.direita = null
+    this.altura = 1
   }
 }
 
-class AVL {
+class AVLTree {
   constructor() {
     this.root = null
   }
 
-  getHeight(node) {
-    return node ? node.height : 0
+  getAltura(node) {
+    return node ? node.altura : 0
   }
 
-  getBalanceFactor(node) {
-    return node ? this.getHeight(node.left) - this.getHeight(node.right) : 0
+  getFatorBalanceamento(node) {
+    return node
+      ? this.getAltura(node.esquerda) - this.getAltura(node.direita)
+      : 0
   }
 
-  updateHeight(node) {
+  atualizarAltura(node) {
     if (node)
-      node.height =
-        Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1
+      node.altura =
+        Math.max(this.getAltura(node.esquerda), this.getAltura(node.direita)) +
+        1
   }
 
-  rotateRight(y) {
-    const x = y.left
-    const T2 = x.right
+  rotacaoDireita(y) {
+    const x = y.esquerda
+    const T2 = x.direita
 
-    x.right = y
-    y.left = T2
+    x.direita = y
+    y.esquerda = T2
 
-    this.updateHeight(y)
-    this.updateHeight(x)
+    this.atualizarAltura(y)
+    this.atualizarAltura(x)
 
     return x
   }
 
-  rotateLeft(x) {
-    const y = x.right
-    const T2 = y.left
+  rotacaoEsquerda(x) {
+    const y = x.direita
+    const T2 = y.esquerda
 
-    y.left = x
-    x.right = T2
+    y.esquerda = x
+    x.direita = T2
 
-    this.updateHeight(x)
-    this.updateHeight(y)
+    this.atualizarAltura(x)
+    this.atualizarAltura(y)
 
     return y
   }
 
-  balanceNode(node) {
-    this.updateHeight(node)
+  balanceNo(node) {
+    this.atualizarAltura(node)
 
-    const balance = this.getBalanceFactor(node)
+    const fb = this.getFatorBalanceamento(node)
 
-    if (balance > 1 && this.getBalanceFactor(node.left) >= 0)
-      return this.rotateRight(node)
+    if (fb > 1 && this.getFatorBalanceamento(node.esquerda) >= 0)
+      return this.rotacaoDireita(node)
 
-    if (balance > 1 && this.getBalanceFactor(node.left) < 0) {
-      node.left = this.rotateLeft(node.left)
-
-      return this.rotateRight(node)
+    if (fb > 1 && this.getFatorBalanceamento(node.esquerda) < 0) {
+      node.esquerda = this.rotacaoEsquerda(node.esquerda)
+      return this.rotacaoDireita(node)
     }
 
-    if (balance < -1 && this.getBalanceFactor(node.right) <= 0)
-      return this.rotateLeft(node)
+    if (fb < -1 && this.getFatorBalanceamento(node.direita) <= 0)
+      return this.rotacaoEsquerda(node)
 
-    if (balance < -1 && this.getBalanceFactor(node.right) > 0) {
-      node.right = this.rotateRight(node.right)
-
-      return this.rotateLeft(node)
+    if (fb < -1 && this.getFatorBalanceamento(node.direita) > 0) {
+      node.direita = this.rotacaoDireita(node.direita)
+      return this.rotacaoEsquerda(node)
     }
 
     return node
   }
 
   insertRecursive(node, value) {
-    if (node === null) return new Node(value)
+    if (node === null) return new No(value)
 
-    if (value < node.value) node.left = this.insertRecursive(node.left, value)
-    else if (value > node.value)
-      node.right = this.insertRecursive(node.right, value)
+    if (value < node.valor)
+      node.esquerda = this.insertRecursive(node.esquerda, value)
+    else if (value > node.valor)
+      node.direita = this.insertRecursive(node.direita, value)
     else return node
 
-    return this.balanceNode(node)
+    return this.balanceNo(node)
   }
 
   insert(value) {
@@ -99,9 +101,9 @@ class AVL {
     let nodeAtual = this.root
 
     while (nodeAtual) {
-      if (nodeAtual.value === value) return true
-      else if (value > nodeAtual.value) nodeAtual = nodeAtual.right
-      else nodeAtual = nodeAtual.left
+      if (nodeAtual.valor === value) return true
+      else if (value > nodeAtual.valor) nodeAtual = nodeAtual.direita
+      else nodeAtual = nodeAtual.esquerda
     }
 
     return false
@@ -110,51 +112,33 @@ class AVL {
   min() {
     let n = this.root
 
-    while (n && n.left) n = n.left
+    while (n && n.esquerda) n = n.esquerda
 
-    return n ? n.value : null
+    return n ? n.valor : null
   }
 
   max() {
     let n = this.root
 
-    while (n && n.right) n = n.right
+    while (n && n.direita) n = n.direita
 
-    return n ? n.value : null
+    return n ? n.valor : null
   }
 
-  printInOrder(node = this.root) {
-    if (node) {
-      this.printInOrder(node.left)
-
-      console.log(node.value)
-
-      this.printInOrder(node.right)
-    }
-  }
-
-  getTreeHeight() {
-    return this.getHeight(this.root)
+  getAlturaArvore() {
+    return this.getAltura(this.root)
   }
 }
 
-const tree = new AVL()
-// tree.insert(10)
-// tree.insert(20)
-// tree.insert(30)
-// tree.insert(40)
-// tree.insert(50)
-// tree.insert(25)
+const tree = new AVLTree()
+tree.insert(30)
+tree.insert(20)
+tree.insert(40)
+tree.insert(10)
+tree.insert(25)
+tree.insert(35)
+tree.insert(50)
 
-tree.insert(11)
-tree.insert(8)
-tree.insert(5)
-tree.insert(4)
-tree.insert(11)
-tree.insert(17)
-tree.insert(18)
-
-tree.printInOrder()
-console.log('Altura:', tree.getTreeHeight())
+console.log('Altura:', tree.getAlturaArvore())
 console.log('Min:', tree.min())
 console.log('Max:', tree.max())
